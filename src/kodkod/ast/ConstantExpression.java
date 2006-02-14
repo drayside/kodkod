8ba2f9e6f20d229d2a3717e1c3412a7622bf30c4
Greg Dennis <gdennis@csail.mit.edu>
2006-02-14 02:04:19 +0000

    optimizations to ConstantExpression

--- src/kodkod/ast/ConstantExpression.java
+++ src/kodkod/ast/ConstantExpression.java
@@ -24,6 +24,51 @@ public final class ConstantExpression extends LeafExpression {
 		super(name, arity);
 	}
 
+	@Override
+	public Formula apply(Multiplicity mult) {
+		if (this == NONE) {
+			switch(mult) {
+			case LONE: return Formula.TRUE;
+			case NO:   return Formula.TRUE;
+			case ONE:  return Formula.FALSE;
+			case SOME: return Formula.FALSE;
+			}
+		}
+		return super.apply(mult);
+	}
+
+	@Override
+	public Expression apply(UnaryExpression.Operator op) {
+		if (this == IDEN && op == UnaryExpression.Operator.TRANSPOSE) {
+			return IDEN;
+		}
+		return super.apply(op);
+	}
+
+	@Override
+	public Expression compose(BinaryExpression.Operator op, Expression expr) {
+		if (this == UNIV) {
+			switch(op) {
+			case UNION:        return UNIV;
+			case INTERSECTION: return expr;
+			}
+		} else if (this == NONE) {
+			switch(op) {
+			case UNION:        return expr;
+			case INTERSECTION: return NONE;
+			}
+		}
+		return super.compose(op, expr);
+	}
+
+	@Override
+	public Formula compose(ComparisonFormula.Operator op, Expression expr) {
+		if (this == NONE && op == ComparisonFormula.Operator.SUBSET) {
+			return Formula.TRUE;
+		}
+		return super.compose(op, expr);
+	}
+
 	/**
      * Accepts the given visitor and returns the result.
      * @see kodkod.ast.Node#accept(kodkod.ast.visitor.ReturnVisitor)
