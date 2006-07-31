5cc7dfc733b3d8b1aabcec6a8645589e4889c32b
Emina Torlak <emina@csail.mit.edu>
2006-07-31 18:16:09 +0000

    *** empty log message ***

--- src/examples/tptp/Quasigroups7.java
+++ src/examples/tptp/Quasigroups7.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package examples.tptp;
 
 import java.util.ArrayList;
@@ -11,10 +8,6 @@ import kodkod.ast.Expression;
 import kodkod.ast.Formula;
 import kodkod.ast.Relation;
 import kodkod.ast.Variable;
-import kodkod.engine.Solution;
-import kodkod.engine.Solver;
-import kodkod.engine.TimeoutException;
-import kodkod.engine.satlab.SATFactory;
 import kodkod.instance.Bounds;
 import kodkod.instance.Instance;
 import kodkod.instance.Tuple;
@@ -23,16 +16,19 @@ import kodkod.instance.TupleSet;
 import kodkod.instance.Universe;
 
 /**
- * A KK encoding of ALG195+1.p from http://www.cs.miami.edu/~tptp/
+ * Contains the relations/axioms common to the problems ALG195+1.p and ALG197+1.p from
+ * from http://www.cs.miami.edu/~tptp/
+ * 
  * @author Emina Torlak
  */
-final class Quasigroups {
-	private final Relation[] e1, e2, h;
-	private final Relation op1, op2, s1, s2;
+public abstract class Quasigroups7 {
+	final Relation[] e1, e2, h;
+	final Relation op1, op2, s1, s2;
+	
 	/**
-	 * Constructs a new instance of Quasigroups.
+	 * Constructs a new instance of Quasigroups7.
 	 */
-	public Quasigroups() {
+	Quasigroups7() {
 		op1 = Relation.ternary("op1");
 		op2 = Relation.ternary("op2");
 		s1 = Relation.unary("s1");
@@ -79,6 +75,7 @@ final class Quasigroups {
 		return opCoversRange(s1, op1);
 	}
 	
+	
 	/**
 	 * Parametrization of axioms 3 and 6.
 	 * @requires s is unary, op is ternary
@@ -116,28 +113,32 @@ final class Quasigroups {
 	}
 	
 	/**
+	 * Parametrization of axioms 12 and 13.
+	 * @requires e's are unary, op is ternary
+	 */
+	abstract Formula ax12and13(Relation[] e, Relation op);
+	
+	/**
+	 * Returns axiom 12.
+	 * @return ax12
+	 */
+	public final Formula ax12() {
+		return ax12and13(e1, op1);
+	}
+
+	/**
+	 * Returns axiom 13.
+	 * @return ax13
+	 */
+	public final Formula ax13() { 
+		return ax12and13(e2, op2);
+	}
+	
+	/**
 	 * Parametrization of axioms 14 and 15.
 	 * @requires e's are unary, op is ternary
 	 */
-	private static Formula ax14and15(Relation[] e, Relation op) {
-		final Expression expr0 = e[5].join(op); // op(e5,...)
-		final Expression expr1 = e[5].join(expr0); // op(e5,e5)
-		final Expression expr2 = expr1.join(expr0); // op(e5,op(e5,e5))
-		final Expression expr3 = expr2.join(expr2.join(op)); // op(op(e5,op(e5,e5)),op(e5,op(e5,e5)))
-		final Expression expr3a = expr3.join(op); // op(op(op(e5,op(e5,e5)),op(e5,op(e5,e5))),...)
-		final Expression expr4 = e[5].join(expr3a); // op(op(op(e5,op(e5,e5)),op(e5,op(e5,e5))),e5)
-		// e0 = op(op(op(e5,op(e5,e5)),op(e5,op(e5,e5))),op(e5,op(e5,e5)))
-		final Formula f0 = e[0].in(expr2.join(expr3a));
-		// e2 = op(op(e5,op(e5,e5)),op(e5,op(e5,e5)))
-		final Formula f2 = e[2].in(expr3);
-		// e3 = op(op(op(e5,op(e5,e5)),op(e5,op(e5,e5))),e5)
-		final Formula f3 = e[3].in(expr4);
-		// e4 = op(e5,op(e5,e5))
-		final Formula f4 = e[4].in(expr2);
-		// e6 = op(op(op(op(e5,op(e5,e5)),op(e5,op(e5,e5))),e5),op(e5,op(e5,e5)))
-		final Formula f6 = e[6].in(expr2.join(expr4.join(op)));
-		return f0.and(f2).and(f3).and(f4).and(f6);
-	}
+	abstract Formula ax14and15(Relation[] e, Relation op);
 	
 	/**
 	 * Returns lines 1 and 3-6 of axiom 14.
@@ -159,28 +160,7 @@ final class Quasigroups {
 	 * Parametrization of axioms 16-22.
 	 * @requires e is unary, h is binary
 	 */
-	private Formula ax16_22(Relation e, Relation h) {
-		final Expression expr0 = e.join(op2); // op2(e,...)
-		final Expression expr1 = e.join(expr0); // op2(e,e)
-		final Expression expr2 = expr1.join(expr0); // op2(e,op2(e,e))
-		final Expression expr3 = expr2.join(expr2.join(op2)); // op2(op2(e,op2(e,e)),op2(e,op2(e,e)))
-		final Expression expr3a = expr3.join(op2); // op2(op2(op2(e,op2(e,e)),op2(e,op2(e,e))),...)
-		final Expression expr4 = e.join(expr3a); // op2(op2(op2(e,op2(e,e)),op2(e,op2(e,e))),e)
-		// h(e10) = op2(op2(op2(e,op2(e,e)),op2(e,op2(e,e))),op2(e,op2(e,e)))
-		final Formula f0 = e1[0].join(h).eq(expr2.join(expr3a));
-		// h(e11) = op2(e,e)
-		final Formula f1 = e1[1].join(h).eq(expr1);
-		// h(e12) = op2(op2(e,op2(e,e)),op2(e,op2(e,e)))
-		final Formula f2 = e1[2].join(h).eq(expr3);
-		// h(e13) = op2(op2(op2(e,op2(e,e)),op2(e,op2(e,e))),e)
-		final Formula f3 = e1[3].join(h).eq(expr4);
-		// h(e14) = op2(e,op2(e,e))
-		final Formula f4 = e1[4].join(h).eq(expr2);
-		// h(e16) = op2(op2(op2(op2(e,op2(e,e)),op2(e,op2(e,e))),e),op2(e,op2(e,e)))
-		final Formula f6 = e1[6].join(h).eq(expr2.join(expr4.join(op2)));
-		
-		return f0.and(f1).and(f2).and(f3).and(f4).and(f6);
-	}
+	abstract Formula ax16_22(Relation e, Relation h);
 	
 	/**
 	 * Returns lines 2-7 of axioms 16-22.
@@ -192,6 +172,14 @@ final class Quasigroups {
 			f = f.and(ax16_22(e2[i], h[i]));
 		}
 		return f;
+	}
+	
+	/**
+	 * Returns the conjunction of all axioms and implicit constraints (decls()).
+	 * @return the conjunction of all axioms and implicit constraints
+	 */
+	public final Formula axioms() {
+		return decls().and(ax2ax7()).and(ax3()).and(ax5ax8()).and(ax6()).and(ax12()).and(ax13()).and(ax14()).and(ax15()).and(ax16_22());
 	}
 	
 	/**
@@ -223,18 +211,10 @@ final class Quasigroups {
 	}
 	
 	/**
-	 * Returns the conjunction of all axioms and implicit constraints (decls()).
-	 * @return the conjunction of all axioms and implicit constraints
+	 * Returns the partial bounds the problem (axioms 1, 4, 9-11).
+	 * @return the partial bounds for the problem
 	 */
-	public final Formula axioms() {
-		return decls().and(ax2ax7()).and(ax3()).and(ax5ax8()).and(ax6()).and(ax14()).and(ax15()).and(ax16_22());
-	}
-	
-	/**
-	 * Returns the bounds the problem (axioms 1, 4, 9-13, second formula of 14-15, and first formula of 16-22).
-	 * @return the bounds for the problem
-	 */
-	public final Bounds bounds() {
+	public Bounds bounds() {
 		final List<String> atoms = new ArrayList<String>(14);
 		for(int i = 0; i < 7; i++)
 			atoms.add("e1"+i);
@@ -259,37 +239,12 @@ final class Quasigroups {
 		// axiom 4
 		final TupleSet op2h = f.area(f.tuple("e20", "e20", "e20"), f.tuple("e26", "e26", "e26"));
 		
-		for(int i = 0; i < 7; i++) {
-			op1h.remove(f.tuple("e1"+i, "e1"+i, "e1"+i)); // axiom 12
-			op2h.remove(f.tuple("e2"+i, "e2"+i, "e2"+i)); // axiom 13
-		}
-		
-		final TupleSet op1l = f.setOf(f.tuple("e15", "e15", "e11")); // axiom 14, line 2
-		final TupleSet op2l = f.setOf(f.tuple("e25", "e25", "e21")); // axiom 15, line 2
-		
-		op1h.removeAll(f.area(f.tuple("e15", "e15", "e10"), f.tuple("e15", "e15", "e16")));
-		op1h.addAll(op1l);
-		
-		op2h.removeAll(f.area(f.tuple("e25", "e25", "e20"), f.tuple("e25", "e25", "e26")));
-		op2h.addAll(op2l);
-		
-		b.bound(op1, op1l, op1h);
-		b.bound(op2, op2l, op2h);
-		
-		final TupleSet high = f.area(f.tuple("e10", "e20"), f.tuple("e14", "e26"));
-		high.addAll(f.area(f.tuple("e16", "e20"), f.tuple("e16", "e26")));
-		
-		// first line of axioms 16-22
-		for(int i = 0; i < 7; i++) {
-			Tuple t = f.tuple("e15", "e2"+i);
-			high.add(t);
-			b.bound(h[i], f.setOf(t), high);
-			high.remove(t);
-		}
+		b.bound(op1, op1h);
+		b.bound(op2, op2h);
 		
 		return b;
 	}
-
+	
 	private static void displayOp(Instance instance, Relation op) {
 		System.out.println("\n"+op+":");
 		final Iterator<Tuple> iter = instance.tuples(op).iterator();
@@ -302,45 +257,17 @@ final class Quasigroups {
 		}
 	}
 	
-	private static void displayh(Instance instance, Relation[] h) {
+	/**
+	 * Prints the values of the op1, op2, and h1-h7 relations
+	 * to standard out.
+	 */
+	void display(Instance instance) {
+		displayOp(instance, op1);
+		displayOp(instance, op2);
 		for(int i = 0; i < 7; i++) {
 			System.out.println("\n"+h[i]+":");
 			System.out.println(instance.tuples(h[i]));
 		}
 	}
-	
-	private static void usage() {
-		System.out.println("java examples.tptp.Quasigroups");
-		System.exit(1);
-	}
-	
-	/**
-	 * Usage: java examples.tptp.Quasigroups
-	 */
-	public static void main(String[] args) {
-	
-		try {
-	
-			final Quasigroups model = new Quasigroups();
-			final Solver solver = new Solver();
-			solver.options().setSolver(SATFactory.MiniSat);
-			final Formula f = model.axioms().and(model.co1().not());
-			final Bounds b = model.bounds();
-			System.out.println(f);
-			final Solution sol = solver.solve(f, b);
-			if (sol.instance()==null) {
-				System.out.println(sol);
-			} else {
-				System.out.println(sol.stats());
-				displayOp(sol.instance(), model.op1);
-				displayOp(sol.instance(), model.op2);
-				displayh(sol.instance(), model.h);
-			}
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NumberFormatException nfe) {
-			usage();
-		}
-	}
+
 }
