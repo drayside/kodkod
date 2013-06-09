f7b27290426a2b8ad76719cd6309a70f92d55dda
Christopher Kleynhans <chris@kleynhans.ca>
2013-06-09 16:21:42 -0400

    Removed the example FooTest.

--- tests/kodkod/multiobjective/TestSuite.java
+++ tests/kodkod/multiobjective/TestSuite.java
@@ -5,7 +5,6 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-  kodkod.multiobjective.FooTest.class,
   kodkod.multiobjective.SmallEndToEndTest.class
 })
 public class TestSuite {
