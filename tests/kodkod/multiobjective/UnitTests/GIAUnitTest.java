ee545a777fb7ba6228f6e59f7eb15177b4574e5b
AtulanZaman <atulan.zaman@gmail.com>
2013-06-11 17:41:22 -0400

    - Updating GIAUnitTest so that it compiles

--- tests/kodkod/multiobjective/UnitTests/GIAUnitTest.java
+++ tests/kodkod/multiobjective/UnitTests/GIAUnitTest.java
@@ -1,16 +1,20 @@
 package kodkod.multiobjective.UnitTests;
 
 import static org.junit.Assert.*;
+import kodkod.multiobjective.Testmodels.MooProblem;
+import kodkod.multiobjective.Testmodels.rooks_3_metrics_2;
 import kodkod.multiobjective.api.MultiObjectiveProblem;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GIAUnitTest {
 	/* Fields */
-	MultiObjectiveProblem problem;
-	
-	private GIAUnitTest (){
-		this.problem = new MultiObjectiveProblem(...);
+	MooProblem problem;
+
+	@Before
+	private void SetUp(){
+		problem = new rooks_3_metrics_2();
 	}
 	
 	@Test
