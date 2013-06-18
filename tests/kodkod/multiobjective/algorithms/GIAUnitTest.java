dcd44541b4c769211bfbad548d44df40ef5f7716
Ming-Ho Yee <mh@mhyee.com>
2013-06-18 17:05:03 -0400

    Merging in latest unit test to refactor branch

---
+++ tests/kodkod/multiobjective/algorithms/GIAUnitTest.java
@@ -0,0 +1,33 @@
+package kodkod.multiobjective.algorithms;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+import kodkod.engine.Solution;
+import kodkod.engine.satlab.SATFactory;
+import kodkod.multiobjective.Testmodels.MooProblem;
+import kodkod.multiobjective.Testmodels.rooks_3_metrics_2;
+import kodkod.multiobjective.*;
+import kodkod.multiobjective.algorithms.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GIAUnitTest {
+	/* Fields */
+	MooProblem problem;
+
+	@Before
+	public void SetUp(){
+		problem = new rooks_3_metrics_2();
+	}
+	
+	@Test
+	public void testSolveFirst() {
+		GuidedImprovementAlgorithm gia = new GuidedImprovementAlgorithm("asdf", new MultiObjectiveOptions());
+		gia.getOptions().setSolver(SATFactory.DefaultSAT4J);
+		gia.getOptions().setSymmetryBreaking(1000);
+		
+		Solution s = gia.solveFirst(problem.getProblem().getConstraints(), problem.getBounds(), problem.getProblem(), problem.getFormula());
+		assertTrue(s.sat());
+	}
+
+}
