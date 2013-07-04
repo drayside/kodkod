a3ee3087756b168ffb74f42f813b215e59c05ada
AtulanZaman <atulan.zaman@gmail.com>
2013-07-04 18:12:39 -0400

    - Removes dead test - updates test suite

--- tests/kodkod/multiobjective/TestSuite.java
+++ tests/kodkod/multiobjective/TestSuite.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
   kodkod.multiobjective.EndtoEnd.TestSuite.class,
-  kodkod.multiobjective.algorithms.TestSuite.class
 })
 public class TestSuite {
 }
