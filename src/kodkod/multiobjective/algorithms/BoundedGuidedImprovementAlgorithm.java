79eb040444c123905d8865f34790ac39140b2a6b
Ming-Ho Yee <mh@mhyee.com>
2013-07-10 19:00:44 -0400

    Updated BoundedGuidedImprovementAlgorithm to work with refactored MultiObjectiveAlgorithm

--- src/kodkod/multiobjective/algorithms/BoundedGuidedImprovementAlgorithm.java
+++ src/kodkod/multiobjective/algorithms/BoundedGuidedImprovementAlgorithm.java
@@ -13,27 +13,12 @@ import kodkod.multiobjective.MultiObjectiveOptions;
 import kodkod.multiobjective.MultiObjectiveProblem;
 import kodkod.multiobjective.Objective;
 import kodkod.multiobjective.concurrency.SolutionNotifier;
-import kodkod.multiobjective.statistics.StatKey;
 import kodkod.multiobjective.statistics.StepCounter;
 
 public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
 
-    /*
-     * turn on this variable to enable the step counter. It will
-     * count how many steps were taken from a base point to a Pareto point
-     * and report the results in a file specified over the variable filename
-     */
-    final boolean doEvaluation = false;
-
-    final String filename;
-
-    final long startTime = System.currentTimeMillis();
-
-    private static final Logger logger = Logger.getLogger(BoundedGuidedImprovementAlgorithm.class.toString());
-
     public BoundedGuidedImprovementAlgorithm(String desc, MultiObjectiveOptions options) {
-        super(desc, options);
-        this.filename = desc.replace("$", "");
+        super(desc, options, Logger.getLogger(BoundedGuidedImprovementAlgorithm.class.toString()));
     }
 
     @Override
@@ -51,7 +36,10 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
         exclusionConstraints.add(problem.getConstraints());
 
         // Throw a dart and get a starting point
-        Solution solution = solveFirst(problem.getConstraints(), problem.getBounds(), problem, null); // re-assigned around the loop
+        Solution solution = getSolver().solve(problem.getConstraints(), problem.getBounds());
+        incrementStats(solution, problem, problem.getConstraints(), true, null);
+
+        solveFirstStats(solution);
 
         List<Formula> boundaries = new ArrayList<Formula>(problem.getObjectives().size());
         MetricPoint currentValues = null;
@@ -62,7 +50,8 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
             Formula boundaryConstraint = null;
             logger.log(Level.FINE, "Optimizing on {0}", objective.toString());
 
-            solution = solveOne(problem.getConstraints(), problem.getBounds(), problem, null);
+            solution = getSolver().solve(problem.getConstraints(), problem.getBounds());
+            incrementStats(solution, problem, problem.getConstraints(), false, null);
 
             // Work up to the boundary of this metric
             while (isSat(solution)) {
@@ -71,7 +60,9 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
 
                 boundaryConstraint = currentValues.objectiveImprovementConstraint(objective);
 
-                solution = solveOne(problem.getConstraints().and(boundaryConstraint), problem.getBounds(), problem, boundaryConstraint);
+                Formula constraint = problem.getConstraints().and(boundaryConstraint);
+                solution = getSolver().solve(constraint, problem.getBounds());
+                incrementStats(solution, problem, constraint, false, null);
             }
             logger.log(Level.FINE, "Found boundary {0}", currentValues.boundaryConstraint(objective));
             boundaries.add(currentValues.boundaryConstraint(objective));
@@ -89,7 +80,9 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
         Formula boundaryConstraints = Formula.and(boundaries);
 
         // Now we can do the regular GIA, but with the boundaries as extra constraints
-        solution = solveOne(problem.getConstraints().and(boundaryConstraints), problem.getBounds(), problem, null);
+        Formula constraint = problem.getConstraints().and(boundaryConstraints);
+        solution = getSolver().solve(constraint, problem.getBounds());
+        incrementStats(solution, problem, constraint, false, null);
 
         counter.countStep();
 
@@ -103,12 +96,13 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
                 final Formula improvementConstraints = currentValues.parametrizedImprovementConstraints();
 
                 previousSolution = solution;
-                solution =  solveOne(problem.getConstraints().and(boundaryConstraints).and(improvementConstraints), problem.getBounds(),  problem, improvementConstraints);
+                constraint = problem.getConstraints().and(boundaryConstraints).and(improvementConstraints);
+                solution = getSolver().solve(constraint, problem.getBounds());
+                incrementStats(solution, problem, constraint, false, improvementConstraints);
 
                 counter.countStep();
             }
-            foundMetricPoint();
-            logger.log(Level.FINE, "Found Pareto point with values: {0}", currentValues.values());
+            foundParetoPoint(currentValues);
 
             if (!options.allSolutionsPerPoint()) {
                 // no magnifying glass
@@ -124,7 +118,9 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
 
             // start looking for next base point
             exclusionConstraints.add(currentValues.exclusionConstraint());
-            solution = solveOne(Formula.and(exclusionConstraints).and(boundaryConstraints), problem.getBounds(), problem, null);
+            constraint = Formula.and(exclusionConstraints).and(boundaryConstraints);
+            solution = getSolver().solve(constraint, problem.getBounds());
+            incrementStats(solution, problem, constraint, false, null);
 
             //count this step but first go to new index because it's a new base point
             counter.nextIndex();
@@ -134,46 +130,5 @@ public class BoundedGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
 
         end(notifier);
         debugWriteStatistics();
-    }
-
-    private void debugWriteStatistics(){
-        StringBuilder sb = new StringBuilder("Solver statistics:\n");
-        sb.append("\t# Sat Call: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_SAT_CALL));
-        sb.append("\n");
-        sb.append("\t# Unsat Call: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_UNSAT_CALL));
-        sb.append("\n");
-
-        sb.append("\t# Total Time in Sat Calls: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_SAT_TIME));
-        sb.append("\n");
-        sb.append("\t# Total Time in Sat Calls Solving: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_SAT_TIME_SOLVING));
-        sb.append("\n");
-        sb.append("\t# Total Time in Sat Calls Translating: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_SAT_TIME_TRANSLATION));
-        sb.append("\n");
-
-        sb.append("\t# Total Time in Unsat Calls: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_UNSAT_TIME));
-        sb.append("\n");
-        sb.append("\t# Total Time in Unsat Calls Solving: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_UNSAT_TIME_SOLVING));
-        sb.append("\n");
-        sb.append("\t# Total Time in Unsat Calls Translating: ");
-        sb.append(this.getStats().get(StatKey.REGULAR_UNSAT_TIME_TRANSLATION));
-        sb.append("\n");
-
-        sb.append("\t# Magnifier Sat Call: ");
-        sb.append(this.getStats().get(StatKey.MAGNIFIER_SAT_CALL));
-        sb.append("\n");
-        sb.append("\t# Magnifier Unsat Call: ");
-        sb.append(this.getStats().get(StatKey.MAGNIFIER_UNSAT_CALL));
-        sb.append("\n");
-        sb.append("\t# Total Time in Magnifier: ");
-        sb.append(this.getStats().get(StatKey.MAGNIFIER_TIME));
-
-        logger.log(Level.FINE, sb.toString());
     }
 }
--- src/kodkod/multiobjective/algorithms/IncrementalGuidedImprovementAlgorithm.java
+++ src/kodkod/multiobjective/algorithms/IncrementalGuidedImprovementAlgorithm.java
@@ -18,12 +18,6 @@ import kodkod.multiobjective.statistics.StepCounter;
 
 public final class IncrementalGuidedImprovementAlgorithm extends MultiObjectiveAlgorithm {
 
-	/*
-	 * turn on this variable to enable the step counter. It will
-	 * count how many steps were taken from a base point to a Pareto point
-	 * and report the results in a file specified over the variable filename
-	 */
-
 	public IncrementalGuidedImprovementAlgorithm(final String desc, final MultiObjectiveOptions options) {
 		super(desc, options, Logger.getLogger(IncrementalGuidedImprovementAlgorithm.class.toString()));
 	}
